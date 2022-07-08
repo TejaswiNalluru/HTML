759d3fbeea36691ce040f10cc18faad00abd018f
TejaswiNalluru <106387995+TejaswiNalluru@users.noreply.github.com>
2022-07-08 06:53:09 -0700

    Add files via upload

--- node_js assg/router/index.js
+++ node_js assg/router/index.js
@@ -1,1 +1,18 @@
+const express =require("express");
+const app=express();
+const router =express.Router()
 
+router.get('/registration',(req,res)=>{
+    res.sendFile(__dirname+"/views/registration.html");
+})
+
+app.get('/',(req,res,next)=>{
+    res.send('Hello from router file')
+})
+
+router.get('/json',(req,res,next)=>{
+    const data={
+        Name:'Tejaswi'
+    }
+    res.json(data)
+})
