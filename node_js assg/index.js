98f93e7ffce1009c98d6be090b13e6f2adaa6700
TejaswiNalluru <106387995+TejaswiNalluru@users.noreply.github.com>
2022-07-08 06:44:54 -0700

    node js code

--- node_js assg/index.js
+++ node_js assg/index.js
@@ -1,1 +1,16 @@
+const express =require("express");
 
+const app=express();
+const path=require('path')
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/registration',(req,res)=>{
+    res.sendFile(__dirname+"/views/registration.html");
+})
+
+app.get('/',(req,res,next)=>{
+    res.send('Hello')
+})
+
+
+app.listen(3000)
